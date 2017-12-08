ae200c5fc0ccb77fda63e0dffa05e1474da21b30
hoppe <jannis.hoppe@rwth-aachen.de>
2017-12-08 15:17:29 +0100

    adjustment make measurements

--- Code/Textfile creation/create_simfile.m
+++ Code/Textfile creation/create_simfile.m
@@ -1,13 +1,14 @@
-sensor_max_distance = 600; %max sensor distance in pixles
-sigma_Q = [1,1,1];
+sensor_max_distance = 500; %max sensor distance in pixles
+sigma_Q = [25,15*2*pi/360];
+disturb_measurements = 1;
 
 true_path = make_true_path();
-load('C:\Users\hoppe\Desktop\KTH\Applied Estimation\Project\Code\Textfile creation\time_vec.mat');
+load('C:\Users\hoppe\Desktop\KTH\Applied Estimation\test\Code\Textfile creation\time_vec.mat');
 t_vec = make_time_vec(time_vec);
 input = make_input(true_path,t_vec);
 
-map = load('C:\Users\hoppe\Desktop\KTH\Applied Estimation\Project\Dataset\Textfiles\MapStanford.txt');
+map = load('C:\Users\hoppe\Desktop\KTH\Applied Estimation\test\Dataset\Textfiles\MapStanford.txt');
 
-[number_landmarks,measurements] = make_measurements(map,true_path,sensor_max_distance);
+[number_landmarks,measurements] = make_measurements(map,true_path,sensor_max_distance,sigma_Q,disturb_measurements);
 
 %outfile = [t_vec, input, true_path, measurements];

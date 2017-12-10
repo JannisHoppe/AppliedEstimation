2ae238e9abc8329f7e2e83980a849e54eecba67c
Thomas Jantos <thomas.jantos@rwth-aachen.de>
2017-12-10 18:10:02 +0100

    predict landmarks finished

--- Code/Other scripts/Calculate_functions.m
+++ Code/Other scripts/Calculate_functions.m
@@ -1,11 +1,26 @@
 clear all;
-syms Mx xx My xy z x_theta z1 z2 real
-
-mu = [Mx; My];
-pos = [xx; xy; x_theta];
-z = [z1 ;z2];
-h = [sqrt((Mx-xx)^2+(My-xy)^2); atan2(My-xy,Mx-xx)-x_theta];
-H = jacobian(h,mu)
+% syms Mx xx My xy z x_theta z1 z2 real
+% 
+% mu = [Mx; My];
+% pos = [xx; xy; x_theta];
+% z = [z1 ;z2];
+% h = [sqrt((Mx-xx)^2+(My-xy)^2); atan2(My-xy,Mx-xx)-x_theta];
+% H = jacobian(h,mu)
 
 %h_inv = solve(z==h,mu)
 
+syms H11 H12 H21 H22 Sig11 Sig12 Sig21 Sig22 Q11 Q12 Q21 Q22 K1 K2 K3 K4 mu1 mu2 zd1 zd2 real
+
+H = [H11 H12;H21 H22];
+Sig = [Sig11 Sig12;Sig21 Sig22];
+H_T = transpose(H);
+Q = [Q11 Q12;Q21 Q22];
+mu = [mu1; mu2];
+z= [zd1;zd2];
+K = [K1 K2; K3 K4];
+I = [1 0; 0 1];
+results = (I-K*H)*Sig
+
+
+
+z'*Q*(z)
--- Code/SLAM/predict_landmarks.m
+++ Code/SLAM/predict_landmarks.m
@@ -1,12 +1,13 @@
 %map_ids -- id's of the landmarks that were seen this time step
-function [S_bar] = predict_landmarks(S,Q_t,map_ids,num_landmarks)
+% z = measurement for this time step
+function [S_bar] = predict_landmarks(S,Q_t,z,map_ids,num_landmarks)
     S_bar = S;
     for j= 1:num_landmarks
         weight_landmarks = [];
         %Feature measured ? 
         if ismember(j,map_ids)
             %Feature seen before?
-            m_idx = 5+(j-1)*6;
+            m_idx = 5+(j-1)*7;
             if(max(S(m_idx,:))==0)%no
                 S_bar(m_idx,:) = 1; %now it was seen
                 %mean : S_bar(m_idx+1:m_idx+2,:)
@@ -14,18 +15,37 @@ function [S_bar] = predict_landmarks(S,Q_t,map_ids,num_landmarks)
                 inv_H = special_mat_inverse(H);
                 S_bar(m_idx+3,:) = Q_t(1)*(inv_H(1,:)).^2 + Q_t(4) * (inv_H(2,:)).^2 ;
                 S_bar(m_idx+4,:) = Q_t(1)* (inv_H(1,:).*inv_H(3,:)) + Q_t(4) *(inv_H(2,:).*inv_H(4,:));
-                S_bar(m_idx+5,:) = Q_t(1) * (inv_H(3,:)).^2 + Q_t(4)*(inv_H(4,:)).^2;
+                S_bar(m_idx+5,:) = Q_t(1)* (inv_H(1,:).*inv_H(3,:)) + Q_t(4) *(inv_H(2,:).*inv_H(4,:));
+                S_bar(m_idx+6,:) = Q_t(1) * (inv_H(3,:)).^2 + Q_t(4)*(inv_H(4,:)).^2;
                 weights_landmarks = [weights_landmarks; ones(1,size(S,2))];
             else
                 z_hat = observation_model_slam(S(1:3,:),S(m_idx+1:m_idx+2,:));
                 H = calculate_jacobian(S(1:3,:),S(m_idx+1:m_idx+2,:));
-                Q = 0;
-                
+                Q = [Q_t(1)+H(1,:).*(H(1,:).*S(m_idx+3,:) + H(2,:).*S(m_idx+5,:)) + H(2,:).*(H(1,:).*S(m_idx+4,:) + H(2,:).*S(m_idx+6,:)); 
+                    H(3,:).*(H(1,:).*S(m_idx+3,:)+ H(2,:).*S(m_idx+5,:)) + H(4,:).*(H(1,:).*S(m_idx+4,:)+ H(2,:).*S(m_idx+6,:));
+                    H(1,:).*(H(3,:).*S(m_idx+3,:)+ H(4,:).*S(m_idx+5,:)) + H(2,:).*(H(3,:).*S(m_idx+4,:)+ H(4,:).*S(m_idx+6,:));
+                    Q_t(4) + H(3,:).*(H(3,:).*S(m_idx+3,:)+ H(4,:).*S(m_idx+5,:)) + H(4,:).*(H(3,:).*S(m_idx+4,:)+ H(4,:).*S(m_idx+6,:))];
+                inv_Q = special_mat_inverse(Q);
+                K = [inv_Q(1,:).*(H(1,:).*S(m_idx+3,:) + H(2,:).*S(m_idx+4,:)) + inv_Q(3,:).*(H(3,:).* S(m_idx+3,:) + H(4,:).*S(m_idx+4,:));
+                    inv_Q(2,:).*(H(1,:).*S(m_idx+3,:) + H(2,:).*S(m_idx+4,:)) + inv_Q(4,:).*(H(3,:).* S(m_idx+3,:) + H(4,:).*S(m_idx+4,:));
+                    inv_Q(1,:).*(H(1,:).*S(m_idx+5,:) + H(2,:).*S(m_idx+6,:)) + inv_Q(3,:).*(H(3,:).* S(m_idx+5,:) + H(4,:).*S(m_idx+6,:));
+                    inv_Q(2,:).*(H(1,:).*S(m_idx+5,:) + H(2,:).*S(m_idx+6,:)) + inv_Q(4,:).*(H(3,:).* S(m_idx+5,:) + H(4,:).*S(m_idx+6,:))];
+                % update mean for landmark
+                S_bar(m_idx+1,:) = S(m_idx+1,:) + K(1,:).* (z(1) - z_hat(1,:)) + K(2,:).*(z(2)-z_hat(2,:));
+                S_bar(m_idx+2,:) = S(m_idx+2,:) + K(3,:).*(z(1) - z_hat(1,:)) + K(4,:).*(z(2)-z_hat(2,:));
+                % update covariance for landmark
+                S_bar(m_idx+3,:) = -S(m_idx+3,:).*(H(1,:).*K(1,:) + H(3,:).*K(2,:) - 1) - S(m_idx+5,:).*(H(2,:).*K(1,:) + H(4,:).*K(2,:)) ;
+                S_bar(m_idx+4,:) = -S(m_idx+4,:).*(H(1,:).*K(1,:) + H(3,:).*K(2,:) - 1) - S(m_idx+6,:).*(H(2,:).*K(1,:) + H(4,:).*K(2,:));
+                S_bar(m_idx+5,:) = -S(m_idx+3,:).*(H(1,:).*K(3,:) + H(3,:).*K(4,:)) - S(m_idx+5,:).*(H(2,:).*K(3,:) + H(4,:).*K(4,:)-1);
+                S_bar(m_idx+6,:) = -S(m_idx+4,:).*(H(1,:).*K(3,:) + H(3,:).*K(4,:)) - S(m_idx+6,:).*(H(2,:).*K(3,:) + H(4,:).*K(4,:)-1);
+                weights_landmarks = [weights_landmarks; weight_feature(z,z_hat,Q)];
+              
+
             end
 %         else
 %             S_bar(5*j:5*j+4,:) = S(5*j:5*j+4,:);
         end
     end
-    
+    S_bar(4,:) = weight_particles(weights_landmarks);
 end
 

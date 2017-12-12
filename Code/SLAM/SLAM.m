4515e8f67758c5f83c2d2f59baeaa8fb650f97f8
hoppe <jannis.hoppe@rwth-aachen.de>
2017-12-12 10:01:56 +0100

    functions renamed

--- Code/SLAM/SLAM.m
+++ Code/SLAM/SLAM.m
@@ -15,7 +15,7 @@
 % Outputs:
 %           S(t)                4XM
 %           outliers            1X1
-function [S,outliers] = mcl(S,R,Q,z,known_associations,v,omega,Lambda_psi,Map_IDS,delta_t,t,USE_KNOWN_ASSOCIATIONS,RESAMPLE_MODE,FIXED_POST_STATION)
+function [S,outliers] = SLAM(S,R,Q,z,known_associations,v,omega,Lambda_psi,Map_IDS,delta_t,t,USE_KNOWN_ASSOCIATIONS,RESAMPLE_MODE,FIXED_POST_STATION)
 
 S_bar_state = S(1:4,:);
 [S_bar_state] = predict_state(S_bar_state,v,omega,R,delta_t);
